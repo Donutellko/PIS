9b161d80c8055736471c06a151dc5847ccb8f3c7
Шергалис Донат Витальевич <dshergalis@alfabank.ru>
2020-09-27 20:22:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+# list = []
+# for i in range(8, 17):
+#     list.append(i)
+# 
+# print(list[5])
+# 
+# print(list[3:6])
+
+# name = "Донат"
+# surname = "Шергалис"
+# 
+# sum = len(name) + len(surname)
+# print(sum)
+# 
+# print(name + " " + surname)
+
+# list = "123456"
+#
+# if (len(list) < 5):
+#     print(list[:len(list)//2])
+# else:
+#     print(list[len(list)//2:])
+
+# number = 3  ** 2
+#
+# a = 1
+# c = 3
+#
+# while c < number:
+#     a += 1
+#     c *= 3
+#
+# if c == number:
+#     print(a)
+# else:
+#     print("неа")
+
+
+# num = 100
+# word = "кек"
+# print("Вадим Андреевич говорит: ")
+# # for i in range(num):
+# #     print(word)
+#
+# for i in word:
+#     print(i * num)
+#
+# for i in range(num - 1):
+#     print(word + " " + str(i))
+
+
+a = "пархоменко"
+b = [c.upper() + "_" for c in a]
+print("".join(b))
