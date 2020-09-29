e1ce98e5b24e562f18d22b74c938ec6716ca2026
Шергалис Донат Витальевич <dshergalis@alfabank.ru>
2020-09-29 19:30:00 +0300

    ht1-3 done (now completely)

--- Домашнее задание/ht1_while.py
+++ Домашнее задание/ht1_while.py
@@ -24,12 +24,13 @@ assert expected == actual
 # Демо
 for i in range(5):
     distance = 10 ** i
-    print("{} км за {} дней".format(distance, task1(distance)))
+    print("    {} км за {} дней".format(distance, task1(distance)))
 
 
 print("""
 Задание 2
-Каждый день я пробегаю «следующее простое число» км. Сколько дней пройдет, пока я в сумме пробегу 1000 км? 10000 км? (дополнительная задача, понять, как отличается от номера 1)
+Каждый день я пробегаю «следующее простое число» км. 
+Сколько дней пройдет, пока я в сумме пробегу 1000 км? 10000 км? (дополнительная задача, понять, как отличается от номера 1)
 """)
 
 
@@ -73,5 +74,93 @@ assert expected == actual
 # Демо
 for i in range(5):
     distance = 10 ** i
-    print("{} км за {} дней".format(distance, task2(distance)))
+    print("    {} км за {} дней".format(distance, task2(distance)))
 
+
+
+print("""
+Задание 3
+Начав тренировки, спортсмен в первый день пробежал 10 км. 
+Для увеличения выносливости ему необходимо повышать норму бега через одну тренировку на 15% от нормы предыдущей тренировки. 
+Спортсмен тренируется каждый день. Какой суммарный путь он пробежит за 30 дней.
+""")
+
+
+def task3(days):
+    i = 1
+    sum = 0
+    perDay = 10
+    mult = 1.15
+    while i <= days:
+        sum += perDay
+        perDay *= mult
+        i += 1
+    return sum
+
+
+# Test
+expected = 10 + 10 * 1.15 + 10 * (1.15 ** 2)
+actual = task3(3)
+assert expected - actual < 0.1
+# end test
+
+# Demo
+print(f"    За 2 дня пробежит {task3(2)} км")
+print(f"    За 30 дней пробежит {task3(30)} км")
+
+
+
+print("""
+Задание 4
+Начав тренировки, спортсмен в первый день пробежал 10 км. 
+Каждый следующий день он увеличивал дневную норму на 10% от нормы предыдущего дня. 
+Через сколько дней:
+а) спортсмен будет пробегать в день больше 20 км;
+""")
+
+
+def task4a(n):
+    i = 0
+    perDay = 10
+    mult = 1.10
+    while perDay <= n:
+        perDay *= mult
+        i += 1
+    return i
+
+
+# Test
+expected = 8
+assert (10 * (1.10 ** expected - 1)) <= 20
+assert (10 * (1.10 ** expected)) > 20
+assert expected == task4a(20)
+# End test
+
+# Demo
+print(f"    Больше 20 км в день через {task4a(20)} дней")
+
+
+print("""
+b) пробежит суммарный путь 100 км.
+""")
+
+
+def task4b(n):
+    i = 0
+    sum = 0
+    perDay = 10
+    mult = 1.10
+    while sum < n:
+        sum += perDay
+        perDay *= mult
+        i += 1
+    return i
+
+
+# Test
+assert 1 == task4b(10)
+assert 2 == task4b(21)
+# End test
+
+# Demo
+print(f"    Пробежит суммарный путь 100 км через {task4b(100)} дней")
--- Домашнее задание/ht2_for.py
+++ Домашнее задание/ht2_for.py
@@ -1,4 +1,91 @@
 print("""
+Задание 1
+Найти N-й член последовательности 1, 1, 2, 3, 5, 8, 13...
+""")
+print("    Примечание: индексы считаю с 1")
+
+
+def task1(n):
+    if n <= 2:
+        return 1
+    a = 1
+    b = 1
+    for i in range(n - 2):
+        c = a + b
+        a = b
+        b = c
+    return b
+
+
+# Test
+assert 1 == task1(1)
+assert 1 == task1(2)
+assert 2 == task1(3)
+assert 3 == task1(4)
+assert 5 == task1(5)
+# end test
+
+# Demo
+print(f"    3  член последовательности: {task1(3)}")
+print(f"    10 член последовательности: {task1(10)}")
+
+
+print("""
+Задание 2
+Найти N-й член последовательности 1, 1, 1, 3, 5, 9, 17...
+""")
+
+
+def task2(n):
+    if n <= 3:
+        return 1
+    a = 1
+    b = 1
+    c = 1
+    for i in range(n - 3):
+        d = a + b + c
+        a = b
+        b = c
+        c = d
+    return c
+
+
+# Test
+assert 1 == task2(1)
+assert 1 == task2(2)
+assert 1 == task2(3)
+assert 3 == task2(4)
+assert 5 == task2(5)
+assert 9 == task2(6)
+# end test
+
+# Demo
+print(f"    4  член последовательности: {task2(4)}")
+print(f"    10 член последовательности: {task2(10)}")
+
+
+print("""
+Задание 3
+Вывести квадраты нечетных чисел до N. (генератором списков)
+""")
+
+
+def task3(n):
+    return [i ** 2 for i in range(1, n + 1, 2)]
+
+
+# Test
+expected = [1, 9, 25, 49]
+actual = task3(7)
+assert expected == actual
+# end test
+
+
+# Demo
+print("N=7: ", task3(7))
+
+
+print("""
 Задание 5
 Даны натуральные числа А и В. Вывести сначала все чётные числа, заключённые
 между ними, потом все нечётные (генератором/ами списков).
